9fe83370b4166d53b54cf6474445159d63f7b482
brian37ful <a2882525224@gmail.com>
2015-04-24 00:08:11 +0800

    Create Lazy_hw_build.sh

---
+++ Lazy_hw_build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Program:
+#   In order to build the god damn OS homework
+# History:
+# 2015/4/23  first release  Author by Brian37ful
+
+test -e dir_list && rm dir_list
+ls > dir_list
+test -e Result && rm Result
+
+while read string
+do
+    test -d $string || continue
+    cd $string || continue
+    echo "\033[34m Building in $string  \033[0m"
+    g++ *.c* && echo "$string Successful" >> ../Result && cd .. && continue
+    echo "$string Failed" >> ../Result
+    cd ..
+done < dir_list
+exit 1
